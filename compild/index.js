ca3c11bb0b7a33f3153306fe6e7c5421b94a1eb4
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-03-04 06:36:49 +0500

    add

--- compild/index.js
+++ compild/index.js
@@ -1,10 +1,39 @@
 "use strict";
 // 1-exam
-class Person {
+class User {
+    name;
+    email;
+    age;
+    constructor(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
 }
-this.title = title;
-this.author = author;
-this.page = page;
-title: string;
-author: string;
-page: number;
+class UserAdmin extends User {
+    name;
+    constructor(name, email, age) {
+        super(name, email, age);
+        this.name = name;
+    }
+}
+class Moderator extends User {
+    constructor(name, email, age, isActive = true) {
+        super(name, email, age);
+    }
+}
+class Admin extends Moderator {
+    constructor(name, email, age, isActive = true) {
+        super(name, email, age, isActive);
+    }
+}
+class SuperAdmin extends Admin {
+    constructor(name, email, isActive = true, age, adminLevel) {
+        super(name, email, age, isActive);
+    }
+}
+const user1 = new User('John', 'John@gmail.com', 26);
+console.log(user1);
--- index.ts
+++ index.ts
@@ -1,11 +1,51 @@
 // 1-exam
-class Person {
-  constructor(title: string, author: string, page: number, this: any)
-  this.title = title;
-this.author = author;
-this.page = page;
+class User {
+  constructor(public name: string, public email: string, public age: number) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
 }
 
-title: string;
-author: string;
-page: number
+class UserAdmin extends User {
+  constructor(public name: string, email: string, age: number) {
+    super(name, email, age);
+  }
+}
+
+class Moderator extends User {
+  constructor(
+    name: string,
+    email: string,
+    age: number,
+    isActive: boolean = true
+  ) {
+    super(name, email, age);
+  }
+}
+class Admin extends Moderator {
+  constructor(
+    name: string,
+    email: string,
+    age: number,
+    isActive: boolean = true
+  ) {
+    super(name, email, age, isActive);
+  }
+}
+
+class SuperAdmin extends Admin {
+  constructor(
+    name: string,
+    email: string,
+    isActive: boolean = true,
+    age: number,
+    adminLevel: number
+  ) {
+    super(name, email, age, isActive);
+  }
+}
+
+const user1 = new User('John', 'John@gmail.com', 26);
+
+console.log(user1);
